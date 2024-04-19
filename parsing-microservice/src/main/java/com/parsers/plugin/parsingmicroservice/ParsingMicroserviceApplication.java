f5d8108a9eda893ac9855316124d753247947957
sverma <sverma@redseal.net>
2024-04-19 15:54:11 +0530

    Initialized parser microservice

---
+++ parsing-microservice/src/main/java/com/parsers/plugin/parsingmicroservice/ParsingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parsers.plugin.parsingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ parsing-microservice/src/test/java/com/parsers/plugin/parsingmicroservice/ParsingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsers.plugin.parsingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
